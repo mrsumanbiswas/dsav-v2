b96ed26a02383c39836a921d08f01d74234226fb
Suman Biswas <mrsumanbiswas0@gmail.com>
2023-07-16 02:13:38 +0530

    update anonymous-routing.module.ts

--- src/app/modules/anonymous/anonymous-routing.module.ts
+++ src/app/modules/anonymous/anonymous-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AcademicsComponent } from './academics/academics.component';
+import { AdmissionsComponent } from './admissions/admissions.component';
+import { AlumniComponent } from './alumni/alumni.component';
+import { CampusComponent } from './campus/campus.component';
+import { ParentsComponent } from './parents/parents.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'academics',
+    component: AcademicsComponent
+  },
+  {
+    path: 'admissions',
+    component: AdmissionsComponent
+  },
+  {
+    path: 'alumni',
+    component: AlumniComponent
+  },
+  {
+    path: 'campus',
+    component: CampusComponent
+  },
+  {
+    path: 'parents',
+    component: ParentsComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
