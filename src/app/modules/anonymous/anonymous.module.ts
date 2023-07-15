5216465d6d1cc71deefbd7823541d61271b429d2
Suman Biswas <mrsumanbiswas0@gmail.com>
2023-07-16 01:52:26 +0530

    update anonymous.module.ts

--- src/app/modules/anonymous/anonymous.module.ts
+++ src/app/modules/anonymous/anonymous.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { AcademicsComponent } from './academics/academics.component';
+import { AdmissionsComponent } from './admissions/admissions.component';
+import { CampusComponent } from './campus/campus.component';
+import { ParentsComponent } from './parents/parents.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { AlumniComponent } from './alumni/alumni.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    AcademicsComponent,
+    AdmissionsComponent,
+    CampusComponent,
+    ParentsComponent,
+    ContactsComponent,
+    AlumniComponent
+  ],
   imports: [
     CommonModule
   ]
