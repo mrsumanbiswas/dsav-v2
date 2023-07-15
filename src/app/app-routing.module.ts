652bedf898a2e9f5678b797f9782b9b013918846
Suman Biswas <mrsumanbiswas0@gmail.com>
2023-07-16 01:43:37 +0530

    update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/anonymous/anonymous.module').then(m => m.AnonymousModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./modules/signed/signed.module').then(m => m.SignedModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
