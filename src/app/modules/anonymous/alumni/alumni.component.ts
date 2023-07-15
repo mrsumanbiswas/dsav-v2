a144cc9be800565f70c1e7f4d03e425609ab5f81
Suman Biswas <mrsumanbiswas0@gmail.com>
2023-07-16 01:51:49 +0530

    create alumni.component.html, alumni.component.scss, alumni.component.spec.ts and alumni.component.ts

---
+++ src/app/modules/anonymous/alumni/alumni.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alumni',
+  templateUrl: './alumni.component.html',
+  styleUrls: ['./alumni.component.scss']
+})
+export class AlumniComponent {
+
+}
